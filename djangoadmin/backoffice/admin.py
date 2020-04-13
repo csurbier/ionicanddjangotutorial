af33043e10861b42c955a1bc1bb01fd600d46f7a
Christophe <csurbier@idevotion.fr>
2020-04-13 17:42:49 +0200

    initialize inline formset

--- djangoadmin/backoffice/admin.py
+++ djangoadmin/backoffice/admin.py
@@ -4,10 +4,12 @@ from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.contrib.admin import AdminSite
 from django.contrib.auth.models import User, Group
+from django.forms.models import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
 class shopplanning_inline(admin.TabularInline):
     model = ShopPlanning
-    extra = 4
+    extra = 7
     max_num=7
 
 
@@ -27,11 +29,72 @@ class ShopAdmin(admin.ModelAdmin):
             return qs.filter(user=request.user)
 
 
+class ProductPlanningFormSet(BaseInlineFormSet):
+
+    def clean(self):
+        super(ProductPlanningFormSet, self).clean()
+
+        for form in self.forms:
+            if not form.is_valid():
+                return
+            if form.cleaned_data and not form.cleaned_data.get('DELETE'):
+                startHour = form.cleaned_data['startHour']
+                endHour = form.cleaned_data['endHour']
+                if endHour <= startHour:
+                    raise ValidationError(
+                        "The end hour should be after the startHour (for day %s)" % form.cleaned_data["day"])
+
+                if startHour >= endHour:
+                    raise ValidationError(
+                        "The startHour should be before the endHour (for day %s)" % form.cleaned_data["day"])
+
+
+
 class productplanning_inline(admin.TabularInline):
     model = ProductPlanning
-    extra = 4
+    extra = 7
     max_num=7
+    formset = ProductPlanningFormSet
 
+    def get_formset(self, request, obj=None, **kwargs):
+        initial = []
+        if request.method == "GET":
+            # get user
+            user = User.objects.get(id=request.user.id)
+            #check if current connected user has a shop
+            shops = Shop.objects.filter(user_id=user.id)
+            if shops:
+                # we assume there is only one shop for a user
+                shop = shops[0]
+                # get planning shop
+                plannings = ShopPlanning.objects.filter(refShop=shop.id)
+                if obj == None:
+                    if plannings:
+                        #no planning set and we have planning for the shop
+                        import datetime
+                        for planning in plannings:
+                            initial.append({
+                                'day': planning.day,
+                                'startHour': planning.startHour,
+                                'endHour': planning.endHour
+
+                            })
+                else:
+                    if plannings:
+                        pass
+                    else:
+                        import datetime
+                        for planning in plannings:
+                            initial.append({
+                                'day': planning.day,
+                                'startHour': planning.startHour,
+                                'endHour': planning.endHour
+
+                            })
+        formset = super(productplanning_inline, self).get_formset(request, obj, **kwargs)
+        from functools import partialmethod
+        formset.__init__ = partialmethod(formset.__init__, initial=initial)
+        return formset
 
 class ProductAdmin(admin.ModelAdmin):
     inlines = [productplanning_inline]
